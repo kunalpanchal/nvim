ec1b0961b2eb1cec0e9a01670d98e455a382a895
Kunal <hello@kunalpanchal.in>
2023-10-15 23:50:18 +0200

    initial commit

---
+++ lua/core/options.lua
@@ -0,0 +1,12 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.colorcolumn = '80,120'
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.guifont = "FiraCode Nerd Font:h15:l"
+
+
+
